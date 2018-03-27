0bb843c6b64164595a8428a0dbd3897dfb4fe388
hzlinannan <hzlinannan@corp.netease.com>
2018-03-27 15:54:47 +0800

    initial commit

---
+++ examples/lint-staged/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "plugins": [
+        
+    ],
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+    },
+    "extends": ["eslint:recommended",  "edu"],
+    "globals": {
+        "NEJ": true,
+        "define": true,
+        "Regular": true
+        "jQuery": true,
+        "$": true
+    }
+};
